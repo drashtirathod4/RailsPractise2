d25034c23af067ebbf5ef56f99f94c51d6011ce2
Drashti Rathod <drashtir9@gmail.com>
2022-03-12 14:31:10 +0530

    feat: Implemented user authentication using devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'rubyonrails2001@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    :user_name => ENV['gmail_username'],
+    :password => ENV['gmail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true 
+  }
 end
